64552fc9176bd07577ecf0b0978a5594a063a1b9
Katya Greimes <katyagreimes@gmail.com>
2024-04-12 21:10:18 -0700

    "Added the form and card generation."

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 const barnCats = alert("let's do this!!")
 console.log(barnCats);
+
+let catArray = ['Whiskers', 'Tom', 'Salem', 'Puss in Boots', 'Binx', 'Sassy'];
+const catGallery = document.getElementById("cat-gallery");
+
+function getCats() {
+    let i = 0;
+    do {
+        const catCard = document.createElement('div');
+        catCard.classList.add("card");
+        catGallery.appendChild(catCard);
+
+        const catImg = document.createElement('div');
+        catImg.classList.add('cat-img');
+        catCard.appendChild(catImg);
+
+        const catName = document.createElement("h3");
+        catName.classList.add('cat-name');
+        catName.textContent = catArray[i];
+        catCard.appendChild(catName);
+
+        const catShelter = document.createElement("p");
+        catShelter.classList.add('cat-shelter');
+        catShelter.textContent = "Local Shelter";
+        catCard.appendChild(catShelter);
+
+
+        i++;
+    } while (i< catArray.length);
+}
